960ddb8c6601daf0809fa882f78be390e021ae8f
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-12-16 21:16:21 +0530

    change: footer and header

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "DevSpec";
+export const SITE_DESCRIPTION =
+  "A straightforward collection of must-have concepts for building developer tools. The blueprint, not the implementation.";
