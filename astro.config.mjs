28ead3d3298360e8eb4fefe77eded97092515791
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-12-17 21:18:26 +0530

    remove: vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,8 @@ import tailwind from '@astrojs/tailwind';
 
 import db from '@astrojs/db';
 
-import vercel from '@astrojs/vercel';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind(), db()],
-  adapter: vercel(),
 });
