9edf6bb8018bedfabf36f486d21f5152549cde8b
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-12-17 22:49:06 +0530

    add: newsletter sign up

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import db from '@astrojs/db';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind(), db()],
 });
